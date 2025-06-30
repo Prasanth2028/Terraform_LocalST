191cb4841376d43eb6590416eed648a71131871b
Prasanth2028 <prasanth.k2828@gmail.com>
2025-06-30 20:05:01 +0530

    updating

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "The name of the resource group."
   type        = string
-  value      = "RG_01_TF"
+  default     = "Resource_Group_Terraform_01"
 }
 
 variable "location" {
