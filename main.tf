24b1e8ac0da17660fdf963a0f5aa38759008cdab
Prasanth2028 <prasanth.k2828@gmail.com>
2025-07-01 14:14:45 +0530

    CheckingPlan

--- main.tf
+++ main.tf
@@ -22,4 +22,5 @@ resource "azurerm_resource_group" "main" {
   for_each = toset(var.resource_group_names)
   name     = each.value
   location = var.location
+  tags    = vars.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,23 @@
 variable "resource_group_names" {
   description = "List of resource group names to create."
   type        = list(string)
-  default     = ["Resource_Group_Terraform_01", "Resource_Group_Terraform_02"]
+  default     = ["Resource_Group_Terraform_01", "Resource_Group_Terraform_02", "Resource_Group_Terraform_03"]
 }
 
 variable "location" {
   description = "The Azure location where the resource groups will be created."
   type        = string
   default     = "East US"
+}
+
+variable "tags" {
+  description = "Tags to apply to the resource groups."
+  type        = map(string)
+  default     = {
+    Environment = "Development"
+    Project     = "Terraform"
+    created_by  = "Terraform Script"
+    purpose     = "Resource Group Creation"
+  }
+  
 }
