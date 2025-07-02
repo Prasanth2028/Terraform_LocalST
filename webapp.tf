e4d0122a41d1df41a38f956596479d7a9500edca
Prasanth2028 <prasanth.k2828@gmail.com>
2025-07-02 21:03:08 +0530

    upd

--- webapp.tf
+++ webapp.tf
@@ -13,4 +13,10 @@ resource "azurerm_linux_web_app" "webapp" {
   location            = azurerm_resource_group.main["Resource_Group_Terraform_02"].location
   resource_group_name = azurerm_resource_group.main["Resource_Group_Terraform_02"].name
   service_plan_id     = azurerm_app_service_plan.webapp_plan.id
+  site_config {
+    always_on = false
+    application_stack {
+      python_version = "3.11"
+    }
+  }
 }
