599601c5ac6852045ddb92e0a36d1e9f90e5efce
Prasanth2028 <prasanth.k2828@gmail.com>
2025-07-09 17:39:11 +0530

    Checking Plan

---
+++ asp.tf
@@ -0,0 +1,15 @@
+#terraform import azurerm_app_service_plan.example /subscriptions/292ccf82-092b-41c5-8723-0bbfad3a4080/resourceGroups/Resource_Group_Terraform_01/providers/Microsoft.Web/serverFarms/ASP-manual-01
+# Import the existing App Service Plan into Terraform state
+resource "azurerm_app_service_plan" "example" {
+  name                = "ASP-manual-01"
+  location            = "Canada Central"
+  resource_group_name = "Resource_Group_Terraform_01"
+  kind                = "Linux"
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+
