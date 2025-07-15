966916983e85ab781fd536b554159238465c5e14
Prasanth2028 <prasanth.k2828@gmail.com>
2025-07-15 19:45:00 +0530

    updating

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,6 +1,6 @@
 module "storageaccount" {
   source                   = "git::https://github.com/ModuleASDA/terraform-StorageAccount.git"
-  name                     = "${local.prefix}-${var.storage_account_name}-01"
+  name                     = "${local.prefix}${var.storage_account_name}01"
   location                 = azurerm_resource_group.main["Resource_Group_Terraform_02"].location
   resource_group_name      = azurerm_resource_group.main["Resource_Group_Terraform_02"].name
   account_tier             = "Standard"
