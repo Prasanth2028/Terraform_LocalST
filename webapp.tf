6654e4d2a7330989388e1de3ff975e750a2dfc34
Prasanth2028 <prasanth.k2828@gmail.com>
2025-07-18 20:49:07 +0530

    Plan

--- webapp.tf
+++ webapp.tf
@@ -9,7 +9,7 @@ resource "azurerm_app_service_plan" "webapp_plan" {
 }
 
 module "webapp" {
-  source                            = "github.com/Prasanth2028/terraform-azurerm-web-app/tree/Testing/Tag"
+  source                            = "git::https://github.com/Prasanth2028/terraform-azurerm-web-app.git?ref=Testing/Tag"
   app_name                          = "my-webapp-terraform-01"
   resource_group_name               = azurerm_resource_group.main["Resource_Group_Terraform_02"].name
   location                          = "westeurope"
