1a73ce667dd1898255536c3de8b7fa7c164bd2b3
Prasanth2028 <prasanth.k2828@gmail.com>
2025-07-18 20:09:42 +0530

    Updating

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,5 +1,5 @@
 module "storageaccount" {
-  source                   = "git::https://github.com/ModuleASDA/terraform-StorageAccount/tree/feature/errorfix.git"
+  source                   = "git::https://github.com/ModuleASDA/terraform-StorageAccount.git?ref=feature/errorfix"
   name                     = "${local.prefix}${var.storage_account_name}01"
   location                 = azurerm_resource_group.main["Resource_Group_Terraform_02"].location
   resource_group_name      = azurerm_resource_group.main["Resource_Group_Terraform_02"].name
