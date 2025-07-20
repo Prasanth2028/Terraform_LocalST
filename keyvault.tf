5a57c0244ddcffe9daaa18b010b7151312638d70
Prasanth2028 <prasanth.k2828@gmail.com>
2025-07-20 22:12:34 +0530

    CheckingPlan

--- keyvault.tf
+++ keyvault.tf
@@ -1,12 +1,12 @@
 module "keyvault" {
   source = "git::https://github.com/ModuleASDA/Key-Vault.git"
 
-  soft_delete_retention_days = 7
+  #soft_delete_retention_days = 7
   purge_protection_enabled   = false
-  sku_name                   = "standard"
+  #sku_name                   = "standard"
   key_vault_name             = "KeyVault-Terraform-01"
-  key_vault_location         = azurerm_resource_group.main["Resource_Group_Terraform_01"].location
-  key_vault_rg_name          = azurerm_resource_group.main["Resource_Group_Terraform_01"].nam
+  key_vault_location         = azurerm_resource_group.main["Resource_Group_Terraform_02"].location
+  key_vault_rg_name          = azurerm_resource_group.main["Resource_Group_Terraform_02"].name
   enable_disk_encryption     = true
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  #tenant_id                  = data.azurerm_client_config.current.tenant_id
 }
