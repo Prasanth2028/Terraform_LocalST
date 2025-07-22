bbc027833b92d720ab9025c0eb0f9be617e85b23
Prasanth2028 <prasanth.k2828@gmail.com>
2025-07-22 19:45:00 +0530

    CheckingPlan

--- keyvault.tf
+++ keyvault.tf
@@ -9,5 +9,5 @@ module "keyvault" {
   key_vault_location     = azurerm_resource_group.main["Resource_Group_Terraform_02"].location
   key_vault_rg_name      = azurerm_resource_group.main["Resource_Group_Terraform_02"].name
   enable_disk_encryption = true
-  #tenant_id                  = data.azurerm_client_config.current.tenant_id
+  #tenant_id                  = data.azurerm_client_config.current.tenant_id 
 }
