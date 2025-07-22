eeb613b0651387b296a448f1d5c0b9e0e9638134
Prasanth2028 <prasanth.k2828@gmail.com>
2025-07-22 19:53:04 +0530

    Check

--- keyvault.tf
+++ keyvault.tf
@@ -1,13 +1,11 @@
 module "keyvault" {
-  #source = "git::https://github.com/ModuleASDA/Key-Vault.git"
-  source = "git@github.com:ModuleASDA/Key-Vault.git"
-
-  #soft_delete_retention_days = 7
+  #source                  = "git::https://github.com/ModuleASDA/Key-Vault.git" used as PAT token 
+  source                   = "git@github.com:ModuleASDA/Key-Vault.git?ref=V-2.0"
   purge_protection_enabled = false
-  #sku_name                   = "standard"
-  key_vault_name         = "KeyVault-Terraform-01"
-  key_vault_location     = azurerm_resource_group.main["Resource_Group_Terraform_02"].location
-  key_vault_rg_name      = azurerm_resource_group.main["Resource_Group_Terraform_02"].name
-  enable_disk_encryption = true
-  #tenant_id                  = data.azurerm_client_config.current.tenant_id 
+  key_vault_sku_name       = "standard"
+  key_vault_name           = "KeyVault-Terraform-01"
+  key_vault_location       = azurerm_resource_group.main["Resource_Group_Terraform_02"].location
+  key_vault_rg_name        = azurerm_resource_group.main["Resource_Group_Terraform_02"].name
+  enable_disk_encryption   = true
+  #tenant_id               = data.azurerm_client_config.current.tenant_id
 }
