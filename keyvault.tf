ef1178693b2d78d131dbf41cc60bdb6313e61fee
Prasanth2028 <prasanth.k2828@gmail.com>
2025-08-18 20:22:17 +0530

    CheckingPlan

--- keyvault.tf
+++ keyvault.tf
@@ -8,18 +8,13 @@ module "keyvault" {
   key_vault_rg_name        = azurerm_resource_group.main["Resource_Group_Terraform_02"].name
   enable_disk_encryption   = true
   #tenant_id               = data.azurerm_client_config.current.tenant_id
-  access_policy = [
-    {
-      tenant_id = "d4963ce2-af94-4122-95a9-644e8b01624d"
-      object_id = "4add16bc-946b-44c2-8716-eb3da5a80366"
-      secret_permissions = [
-        "Get",
-        "List",
-        "Set",
-        "Delete",
-        "Purge",
-        "Recover"
-      ]
-    }
-  ]
+}
+
+resource "azurerm_key_vault_access_policy" "second_policy" {
+  key_vault_id = module.keyvault.key_vault_id
+  tenant_id    = "d4963ce2-af94-4122-95a9-644e8b01624d"
+  object_id    = "4add16bc-946b-44c2-8716-eb3da5a80366"
+
+  key_permissions = ["List"]
+  secret_permissions = ["Get", "List"]
 }
