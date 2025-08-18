635d5982531f4c887214521e9c80444122d4fcf2
Prasanth2028 <prasanth.k2828@gmail.com>
2025-08-18 20:50:06 +0530

    Checking

--- variables.tf
+++ variables.tf
@@ -77,10 +77,35 @@ variable "vms" {
       version   = string
       })),
     tags = optional(map(string))
-  }))
+    network_interface_ids = optional(list(string))}))
   default = [
     {
       name                = "vm-terraform-02"
+      size                = "Standard_B1s"
+      admin_username      = "azureuser"
+      admin_password      = "P@ssw0rd1234!"
+      subnet_id           = ""
+      image_os            = "windows"
+      os_simple           = "WindowsServer"
+      os_disk = {
+        caching              = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+      }
+      source_image_reference = {
+        publisher = "MicrosoftWindowsServer"
+        offer     = "WindowsServer"
+        sku       = "2019-Datacenter"
+        version   = "latest"
+      }
+      tags = {
+        environment = "Production"
+        project     = "Terraform"
+        created_by  = "Terraform Script"
+        purpose     = "Virtual Machine Creation"
+      }
+    },
+    {
+      name                = "vm-terraform-03"
       size                = "Standard_B1s"
       admin_username      = "azureuser"
       admin_password      = "P@ssw0rd1234!"
